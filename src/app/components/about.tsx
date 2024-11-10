1edb49e92d7812f74845d10ddcfaaf04e420ba29
Subra Ansari <164074216+subraansari@users.noreply.github.com>
2024-11-10 21:27:18 +0500

    Update about.tsx

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -8,12 +8,12 @@ const About = () => {
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 p-20 md:mb-0">
             <Image
-              className="object-cover object-center rounded mx-auto w-[300px] h-[400px]"
-              alt="hero"
-              src={require("/public/image.jpg")}
-              width={700}
-              height={500}
-            />
+  src="/image.jpg"
+  alt="hero"
+  width={500}
+  height={500}
+  className="object-cover object-center rounded mx-auto w-[15rem]"
+/>
           </div>
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
